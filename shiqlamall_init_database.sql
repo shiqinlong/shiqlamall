3260f68b1dd0652991107eb4387843fc7556d410
qinlong.shi <qinlong.shi@sap.com>
2020-05-06 21:37:29 +0800

    init sql  file

---
+++ shiqlamall_init_database.sql
@@ -0,0 +1,11 @@
+drop database if exists shiqlamall_sms;
+drop database if exists shiqlamall_ums;
+drop database if exists shiqlamall_oms;
+drop database if exists shiqlamall_pms;
+drop database if exists shiqlamall_wms;
+
+create database shiqlamall_sms;
+create database shiqlamall_ums;
+create database shiqlamall_oms;
+create database shiqlamall_pms;
+create database shiqlamall_wms;
